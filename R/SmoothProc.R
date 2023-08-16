e129fdb0a099e84b9305ddce78ae0c5a76a0c6ec
凱(Kai) <yukai.yang@gmail.com>
2023-08-17 01:01:47 +0200

    Update SmoothProc.R make indents

--- R/SmoothProc.R
+++ R/SmoothProc.R
@@ -1,4 +1,3 @@
-SmoothProc<-function(X,Gam,Bet,G,h0,X0,u)
 # All must be matrix data type!
 #X  : T   by s   matrix representing regressors
 #Gam: p+q by q   matrix representing parameters of states (H' F')'
@@ -11,38 +10,38 @@ SmoothProc<-function(X,Gam,Bet,G,h0,X0,u)
 #output
 #Z  : T   by p   smoothed observations
 #h  : T   by q   smoothed underlying process
+SmoothProc<-function(X,Gam,Bet,G,h0,X0,u)
 {
-
-T=dim(X)[1]
-q=dim(Gam)[2]
-p=dim(Gam)[1]-q
-s=dim(Bet)[2]
-if(T<2) return(0)
-if(dim(X)[2]!=s) return(0)
-if(dim(Bet)[1]!=(p+q)) return(0)
-if(dim(G)[1]!=(p+q)) return(0)
-if(dim(G)[2]!=(p+q)) return(0)
-if(dim(h0)[1]!=q) return(0)
-if(dim(h0)[2]!=1) return(0)
-if(dim(X0)[1]!=s) return(0)
-if(dim(X0)[2]!=1) return(0)
-
-
-h=array(0,dim=c(T,q))
-e=u# smoothed innovations
-Z=array(0,dim=c(T,p))# data
-
-#Start
-
-temp=c(Gam%*%h0+Bet%*%X0+G%*%rnorm(p+q))
-h[1,]=temp[(p+1):(p+q)]
-for(i in 1:(T-1)){
-temp=c(Gam%*%h[i,]+Bet%*%X[i,]+G%*%e[i,])
-Z[i,]=temp[1:p]
-h[(i+1),]=temp[(p+1):(p+q)]
-}
-temp=c(Gam%*%h[T,]+Bet%*%X[T,]+G%*%e[T,])
-Z[T,]=temp[1:p]
-
-return(list(Z=Z,h=h))
+  T=dim(X)[1]
+  q=dim(Gam)[2]
+  p=dim(Gam)[1]-q
+  s=dim(Bet)[2]
+  if(T<2) return(0)
+  if(dim(X)[2]!=s) return(0)
+  if(dim(Bet)[1]!=(p+q)) return(0)
+  if(dim(G)[1]!=(p+q)) return(0)
+  if(dim(G)[2]!=(p+q)) return(0)
+  if(dim(h0)[1]!=q) return(0)
+  if(dim(h0)[2]!=1) return(0)
+  if(dim(X0)[1]!=s) return(0)
+  if(dim(X0)[2]!=1) return(0)
+  
+  
+  h=array(0,dim=c(T,q))
+  e=u# smoothed innovations
+  Z=array(0,dim=c(T,p))# data
+  
+  #Start
+  
+  temp=c(Gam%*%h0+Bet%*%X0+G%*%rnorm(p+q))
+  h[1,]=temp[(p+1):(p+q)]
+  for(i in 1:(T-1)){
+    temp=c(Gam%*%h[i,]+Bet%*%X[i,]+G%*%e[i,])
+    Z[i,]=temp[1:p]
+    h[(i+1),]=temp[(p+1):(p+q)]
+  }
+  temp=c(Gam%*%h[T,]+Bet%*%X[T,]+G%*%e[T,])
+  Z[T,]=temp[1:p]
+  
+  return(list(Z=Z,h=h))
 }
