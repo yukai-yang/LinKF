7e18fcd27e2b4234b6be998dc7e5dfd837d255c3
Yukai Yang <yukai.yang@gmail.com>
2023-08-19 21:47:03 +0200

    make manuals

--- R/LinKF.R
+++ R/LinKF.R
@@ -1,30 +1,40 @@
-#' Linear Kalman Filter
+#' Linear Kalman filtering
 #'
-# the model:
-# (z_t', h_{t+1}') = Gam h_t + Bet x_t + G e_t
-# where Gam = (H', F')', Bet = (A', B')', G = (R', Q')'
+#' \code{LinKF} implements the the linear Kalman filtering algorithm.
 #'
-# All must be matrix data type!
-#mZ  : iT   by ip   matrix representing observations
-#mX  : iT   by iu   matrix representing regressors
-#Gam: ip+iq by iq   matrix representing parameters of states (H' F')'
-#Bet: ip+iq by iu   matrix ... parameters of regressors (A' B')'
-#mG  : ip+iq by ip+iq matrix ... parameters of errors (R' Q')'
-#h0 : iq   by 1   matrix ... accurate initial state
-#P0 : iq   by iq   matrix ... accurate initial state's cov
-#mX0 : iu   by 1   matrix ... x at time step 0
-#
-#output
-#h1 : iT   by iq   expectation of h a priori
-#h2 : iT   by iq   expectation of h a posteriori
-#P1 : Tqq array  variance of h a priori
-#P2 : Tqq array  variance of h a posteriori
-#v  : iT   by ip   residuals in measurement eq.
-#S  : Tpp array  covariance of z
-#K  : Tqp array  Kalman gain
+#' The function takes several arguments that specify the linear state space model as well as the data
+#' and returns a list consisting of the estimates from the Kalman filtering algorithm.
+#'
+#' The corresponding linear state space model is as follows:
+#'
+#' \deqn{(z_t', h_{t+1}') = Gam h_t + Bet x_t + G e_t,}
+#' where \eqn{Gam = (H', F')'}, \eqn{Bet = (A', B')'}, and \eqn{G = (R', Q')'}
+#'
+#' @param mZ a T by p matrix representing observations
+#' @param mX a T by u matrix representing explanatory variables
+#' @param Gam a p+q by q matrix representing the coefficients $(H' F')'$ of the latent states
+#' @param Bet a p+q by u matrix representing the coefficients $(A' B')'$ of $X$
+#' @param mG a p+q by p+q matrix representing the coefficients of errors $(R' Q')'$
+#' @param h0 a q by 1 matrix representing the initial state
+#' @param P0 a q by q matrix representing the initial state's covariance matrix
+#' @param mX0 a u by 1  matrix representing $x_0$ at time 0
+#'
+#' @returns A list consisting of the following components:
+#'
+#' \item{h1}{T by q the estimated conditional expectations of the latent states given t-1 (a priori)}
+#' \item{h2}{T by q the estimated conditional expectations of the latent states given t (a posteriori)}
+#' \item{P1}{T by q by q array of covariances of the a priori states}
+#' \item{P2}{T by q by q array of covariances of the a posteriori states}
+#' \item{v}{T by p residuals in the measurement eq.}
+#' \item{S}{T by p by p array of covariances of z}
+#' \item{K}{T by q by p array of Kalman gains}
+#'
+#' @author Yukai Yang, \email{yukai.yang@@statistik.uu.se}
+#'
+#' @export
 LinKF<-function(mZ,mX,Gam,Bet,mG,h0,P0,mX0)
 {
-  
+
   #Initial
   iT=dim(mZ)[1]
   ip=dim(mZ)[2]
@@ -43,16 +53,16 @@ LinKF<-function(mZ,mX,Gam,Bet,mG,h0,P0,mX0)
   if(dim(P0)[2]!=iq) return(0)
   if(dim(mX0)[1]!=iu) return(0)
   if(dim(mX0)[2]!=1) return(0)
-  
+
   h1=array(0,dim=c(iT,iq))# h_t|t-1
   P1=array(0,dim=c(iT,iq,iq))# P_t|t-1
-  
+
   v=array(0,dim=c(iT,ip))# residual in z eq.
   S=array(0,dim=c(iT,ip,ip))# cov of residual
   K=array(0,dim=c(iT,iq,ip))# Kalman gain
   h2=array(0,dim=c(iT,iq))# h_t|t
   P2=array(0,dim=c(iT,iq,iq))# P_t|t
-  
+
   F=Gam[(ip+1):(ip+iq),]
   B=Bet[(ip+1):(ip+iq),]
   temp=mG[(ip+1):(ip+iq),]
@@ -63,9 +73,9 @@ LinKF<-function(mZ,mX,Gam,Bet,mG,h0,P0,mX0)
   temp=mG[1:ip,]
   dim(temp)=c(ip,(ip+iq))
   RR=temp%*%t(temp)
-  
+
   #Start
-  
+
   #Predict
   h1[1,]=F%*%h0+B%*%mX0
   P1[1,,]=F%*%P0%*%t(F)+QQ
@@ -75,7 +85,7 @@ LinKF<-function(mZ,mX,Gam,Bet,mG,h0,P0,mX0)
   K[1,,]=P1[1,,]%*%t(H)%*%solve(S[1,,])
   h2[1,]=h1[1,]+c(K[1,,]%*%v[1,])
   P2[1,,]=(diag(1,iq)-K[1,,]%*%H)%*%P1[1,,]
-  
+
   for(i in 2:iT){
     #Predict
     h1[i,]=F%*%h2[(i-1),]+B%*%mX[(i-1),]
@@ -87,6 +97,6 @@ LinKF<-function(mZ,mX,Gam,Bet,mG,h0,P0,mX0)
     h2[i,]=h1[i,]+c(K[i,,]%*%v[i,])
     P2[i,,]=(diag(1,iq)-K[i,,]%*%H)%*%P1[i,,]
   }
-  
+
   return(list(h1=h1,h2=h2,P1=P1,P2=P2,v=v,S=S,K=K))
 }
--- R/SimKF.R
+++ R/SimKF.R
@@ -1,20 +1,33 @@
-# All must be matrix data type!
-#
-# the model:
-# (z_t', h_{t+1}') = Gam h_t + Bet x_t + G e_t
-# where Gam = (H', F')', Bet = (A', B')', G = (R', Q')'
-#
-#mX  : iT   by iu   matrix representing regressors
-#Gam : ip+iq by iq   matrix representing parameters of states (H' F')'
-#Bet : ip+iq by iu   matrix ... parameters of regressors (A' B')'
-#mG  : ip+iq by ip+iq matrix ... parameters of errors (R' Q')'
-#h0  : iq   by 1   matrix ... initial state
-#mX0 : iu   by 1   matrix ... x at time step 0
-#
-#output
-#Z  : iT   by ip   observations
-#h  : iT   by iq   underlying process
-SimKF<-function(mX,Gam,Bet,mG,h0,mX0)
+#' Simulate from a linear state space model.
+#'
+#' \code{SimKF} implements the simulation from a linear state space model.
+#'
+#' The function takes several arguments that specify the linear state space model
+#' and returns a list consisting of the simulated latent process and the observations.
+#'
+#' The corresponding linear state space model is as follows:
+#'
+#' \deqn{(z_t', h_{t+1}') = Gam h_t + Bet x_t + G e_t,}
+#' where \eqn{Gam = (H', F')'}, \eqn{Bet = (A', B')'}, and \eqn{G = (R', Q')'}
+#'
+#' The seed can be set before running the function.
+#'
+#' @param mX  a T by u matrix representing the explanatory variable $X$
+#' @param Gam a p+q by q matrix representing the coefficients $(H' F')'$ of the latent states
+#' @param Bet a p+q by u matrix representing the coefficients $(A' B')'$ of $X$
+#' @param mG a p+q by p+q matrix representing the coefficients of errors $(R' Q')'$
+#' @param h0 a q by 1 matrix representing the initial state
+#' @param mX0 a u by 1  matrix representing $x_0$ at time 0
+#'
+#' @returns A list consisting of the following components:
+#'
+#' \item{Z}{T by p  the observations}
+#' \item{h}{T by q the latent process of the states}
+#'
+#' @author Yukai Yang, \email{yukai.yang@@statistik.uu.se}
+#'
+#' @export
+SimKF <- function(mX,Gam,Bet,mG,h0,mX0)
 {
   iT=dim(mX)[1]
   iq=dim(Gam)[2]
@@ -30,14 +43,14 @@ SimKF<-function(mX,Gam,Bet,mG,h0,mX0)
   if(dim(h0)[2]!=1) return(0)
   if(dim(mX0)[1]!=iu) return(0)
   if(dim(mX0)[2]!=1) return(0)
-  
-  
+
+
   h=matrix(0,iT,iq)# true states
   e=matrix(rnorm(iT*(ip+iq)),iT,(ip+iq))# innovations
   mZ=matrix(0,iT,ip)# data
-  
+
   #Start
-  
+
   temp=c(Gam%*%h0+Bet%*%mX0+mG%*%rnorm(ip+iq))
   h[1,]=temp[(ip+1):(ip+iq)]
 
@@ -49,6 +62,6 @@ SimKF<-function(mX,Gam,Bet,mG,h0,mX0)
 
   temp=c(Gam%*%h[iT,]+Bet%*%mX[iT,]+mG%*%e[iT,])
   mZ[iT,]=temp[1:ip]
-  
+
   return(list(Z=mZ,h=h))
 }
